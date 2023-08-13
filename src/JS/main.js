0e365b50ee5db361494e1156abad418ac5550432
Emiliano Fernández <emilianofh02@gmail.com>
2023-08-13 16:22:07 -0700

    First commit

---
+++ src/JS/main.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#7d88e5";
+ctx.fillRect(0,0, 100, 100);
+ctx.fillStyle = "#cd0009";
+ctx.fillRect(80,80, 100, 100);
+ctx.fillStyle = "rgba(125, 136, 229, 0.65)";
+ctx.fillRect(160, 160, 100, 100);
