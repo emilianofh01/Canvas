c43466eb638f0553498c9bb719c2ffa25448b846
Emiliano Fernández <emilianofh02@gmail.com>
2023-08-14 09:33:07 -0700

    Ejercicio 3

--- src/JS/main.js
+++ src/JS/main.js
@@ -7,3 +7,17 @@ ctx.fillStyle = "#cd0009";
 ctx.fillRect(80,80, 100, 100);
 ctx.fillStyle = "rgba(125, 136, 229, 0.65)";
 ctx.fillRect(160, 160, 100, 100);
+
+
+// Ejercicio 3
+ctx.beginPath();
+ctx.moveTo(400,300);
+ctx.lineTo(450, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(300, 300);
+ctx.lineTo(300, 450);
+ctx.lineTo(450, 450);
+ctx.lineTo(350, 350);
+ctx.fill();
